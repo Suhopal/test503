de93142ddd2e371e5ffbb0b997e8b09e5236d507
LeeYoungMoo <suhoi@naver.com>
2020-05-23 16:58:28 +0900

    .....

---
+++ test503/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  test503
+//
+//  Created by admin on 23/05/2020.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
