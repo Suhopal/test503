1d963a7afc250f5b9b44dd1eedded791eda61bd7
LeeYoungMoo <suhoi@naver.com>
2020-05-24 10:09:41 +0900

    ..

---
+++ test503/Classes2/New2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  New2ViewController.swift
+//  test503
+//
+//  Created by admin on 24/05/2020.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class New2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
