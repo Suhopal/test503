2292ad866daa1c846007ddb498488150a04967ea
LeeYoungMoo <suhoi@naver.com>
2020-05-24 10:15:15 +0900

    ..

--- test503/ViewController.swift
+++ test503/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("ViewController new")
+        print("ViewController new add ")
     }
 
 
