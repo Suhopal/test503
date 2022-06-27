fdff484715f9cd3fd788a79f326b3d0d297e4777
Suhopal <suhopal@gmail.com>
2022-06-27 17:58:19 +0900

    Add Files MCSManager 832

---
+++ test503/Classes2/MCSManager.swift
@@ -0,0 +1,26 @@
+//
+//  MCSManager.swift
+//   
+//
+//  Created by Suhopal on 2022/01/12.
+//
+
+import Foundation
+import UIKit
+
+
+ 
+
+class MCSManager {
+    
+    static let shared = MCSManager()
+    
+    /// MCSManager.shared.previewPath
+    var isTime                  = false
+     
+    
+    var previewPath       = "good123"
+
+    
+}
+
